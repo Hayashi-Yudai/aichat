066cc48f777a4550abba616263d3659e689e258b
Yudai Hayashi <yudai@wantedly.com>
2025-04-12 13:18:45 +0900

    Remove CHANGE_AGENT topic from Topics enum

--- aichat/topics.py
+++ aichat/topics.py
@@ -5,6 +5,5 @@ class Topics(Enum):
     START_SUBMISSION = auto()
     SUBMIT_MESSAGE = auto()
     UPDATE_CHAT = auto()
-    CHANGE_AGENT = auto()
     PAST_CHAT_RESTORED = auto()
     NEW_CHAT = auto()
